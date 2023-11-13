b19d356abc535677456ed7419a0c14a87e5ae524
yonathan wiezel <yswiezel@gmail.com>
2023-11-13 14:22:34 +0200

    fix layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -76,18 +76,4 @@ const Home = () => {
 
 export default Home;
 
-// import {  useNavigate } from "react-router-dom"
 
-// const Home = () => {
-
-//     const navigate = useNavigate()
-
-//   return (
-//     <div>
-//       Home
-//       <button onClick={()=> navigate("/products")}>products</button>
-//     </div>
-//   )
-// }
-
-// export default Home
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,17 @@
-import { useNavigate } from "react-router-dom"
-import Categories from "../components/Categories"
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { GlobalState, Product } from "../state";
+import {
+  Box,
+  Container,
+  Typography,
+  Grid,
+  Card,
+  CardMedia,
+  Button,
+  CardActions,
+  CardContent,
+} from "@mui/material";
 
 const Products = () => {
   const navigate = useNavigate();
@@ -9,11 +21,68 @@ const Products = () => {
 
   return (
     <div>
-        Products
-        <button onClick={()=> navigate("/product")}>product</button>
-
-
-        <Categories/>
+      Products
+      <button onClick={() => navigate("/product")}>product</button>
+      <div>
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Categories
+            </Typography>
+          </Container>
+          <Container sx={{ py: 8 }} maxWidth="md">
+            <Grid container spacing={4}>
+              {data.map((card: Product, i: number) => (
+                <Grid item key={Date.now() * i} xs={12} sm={6} md={4}>
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      sx={{ height: 140 }}
+                      image="https://source.unsplash.com/random"
+                      title="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h6" component="div">
+                        {card.category}
+                      </Typography>
+                      <Typography
+                        gutterBottom
+                        variant="h5"
+                        component="div"
+                        sx={{ minHeight: 70 }}
+                      >
+                        {card.name}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="text.secondary"
+                        sx={{ minHeight: 60 }}
+                      >
+                        {card.commonAttributes.description}
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Share</Button>
+                      <Button size="small">Learn More</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </Box>
+      </div>
     </div>
   );
 };
