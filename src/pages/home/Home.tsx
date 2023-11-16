f18948068012829ef6209da954105d34a7e6d0c1
yonathan wiezel <yswiezel@gmail.com>
2023-11-16 14:03:29 +0200

    save

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,116 +1,40 @@
-import "./Home.css";
 import { useNavigate } from "react-router-dom";
-import { setCategory } from "../../state";
-import { Box, Container, Typography, Grid } from "@mui/material";
 import {
-  useGetCategoriesQuery,
-  useGetProductsQuery,
-  useSetClicksMutation,
-} from "../../state/api";
+  Box,
+  Container,
+  Typography,
+  Grid,
+} from "@mui/material";
 import Box2 from "@mui/joy/Box";
 import Card2 from "@mui/joy/Card";
 import CardCover2 from "@mui/joy/CardCover";
 import CardContent2 from "@mui/joy/CardContent";
 import Typography2 from "@mui/joy/Typography";
 import { useDispatch } from "react-redux";
-import _ from "lodash";
-import { useState } from "react";
+import './Home.css'
+import { useGetCategoriesQuery } from "../../state/api";
+import { setCategory } from "../../state";
 
 const Home = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  // const data = useSelector(
+  //   (state: { global: GlobalState }) => state.global.products
+  // );
+  // console.log(data);
 
-  const [clickInfo, setClickInfo] = useState({ id: "", product: {} });
-
-  const [updateProduct] = useSetClicksMutation();
-
-  const handleClicks =  async () => {
-    console.log(clickInfo, "info");
-    await updateProduct({ id: clickInfo.id, product: clickInfo.product });
-  };
+  //state-filter
 
   const { data: categories, error } = useGetCategoriesQuery() || []; //categories
-  const { data: products } = useGetProductsQuery({}) || []; //products
   if (error) {
     // Handle the error, e.g., display an error message
     return <div>Error loading categories</div>;
   }
 
   //filter
-  const topCategories = categories
-    ? _.sortBy(categories, "numberOfClicks").slice(0, 3)
-    : [];
-
-  const topProducts = products
-    ? _.sortBy(products, "numberOfClicks").slice(0, 1)
-    : [];
-
-  console.log(topProducts);
 
   return (
     <div style={{ backgroundColor: "#87CEEB" }}>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <div
-          style={{
-            width: "80%",
-            height: "10vh",
-            background: "yellow",
-            marginTop: "4rem",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-around",
-              alignItems: "center",
-            }}
-          >
-            {categories &&
-              topCategories?.map((c: any) => {
-                return (
-                  <div
-                    style={{
-                      height: "10vh",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                    key={Date.now() * Math.random()}
-                  >
-                    {c.category}
-                  </div>
-                );
-              })}
-          </div>
-        </div>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "6rem",
-        }}
-      >
-        <div
-          style={{
-            width: "40vw",
-            background: "coral",
-            height: "4rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <h2> Top Product: {topProducts ? topProducts[0]?.name : ""}</h2>
-        </div>
-      </div>
       <Box
         sx={{
           pt: 8,
@@ -129,56 +53,46 @@ const Home = () => {
           </Typography>
         </Container>
         <Container sx={{ py: 8, width: "80%", margin: "0 auto" }} maxWidth="md">
-          <Grid container spacing={4}>
-            {categories && // category - price
-              categories.map((card: any, i: number) => (
-                <Grid item key={Date.now() * i} xs={12} sm={6} md={4}>
-                  <div
-                    onClick={() => {
-                      dispatch(setCategory(card.category));
-                      let temp = card;
-                      temp = { ...card, numberOfClicks: card?.numOfClicks + 1 };
-                      setClickInfo((prev) => {
-                        console.log(prev, "prev");
-                        return { id: String(temp._id), product: temp };
-                      });                 
-                      handleClicks();
-                      navigate("/products");
-                    }}
-                  >
-                    <Box2
-                      sx={{
-                        perspective: "1000px",
-                        transition: "transform 0.4s",
-                        "& > div, & > div > div": {
-                          transition: "inherit",
-                        },
-                        "&:hover": {
-                          "& > div": {
-                            transform: "rotateY(30deg)",
-                            "& > div:nth-child(2)": {
-                              transform:
-                                "scaleY(0.9) translate3d(20px, 30px, 40px)",
-                            },
-                            "& > div:nth-child(3)": {
-                              transform: "translate3d(45px, 50px, 40px)",
-                            },
-                          },
-                        },
-                      }}
-                    >
-                      <Card2
-                        variant="outlined"
-                        sx={{
-                          minHeight: "280px",
-                          width: 320,
-                          backgroundColor: "#fff",
-                          borderColor: "#000",
-                          backgroundImage: `url(${card.img})`, // Set background image
-                          backgroundSize: "cover",
-                          backgroundPosition: "center",
-                        }}
-                      >
+  <Grid container justifyContent="center">
+    {categories &&
+      categories.map((card: any, i: number) => (
+        <Grid item key={Date.now() * i} xs={6} sm={3} sx={{ marginX: "15px", marginY: "15px" }}> {/* הגדרת ארבע תמונות בכל שורה */}
+          <div onClick={() => {
+              dispatch(setCategory(card.category))
+              navigate("/products");
+            }}>
+            <Box2
+              sx={{
+                perspective: "1000px",
+                transition: "transform 0.4s",
+                "& > div, & > div > div": {
+                  transition: "inherit",
+                },
+                "&:hover": {
+                  "& > div": {
+                    transform: "rotateY(30deg)",
+                    "& > div:nth-child(2)": {
+                      transform: "scaleY(0.9) translate3d(20px, 30px, 40px)",
+                    },
+                    "& > div:nth-child(3)": {
+                      transform: "translate3d(45px, 50px, 40px)",
+                    },
+                  },
+                },
+              }}
+            >
+              <Card2
+                variant="outlined"
+                sx={{
+                  minHeight: "280px",
+                  width: "100%", // הגדרת רוחב התמונה ל-100% של התא
+                  backgroundColor: "#fff",
+                  borderColor: "#000",
+                  backgroundImage: `url(${card.img})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                }}
+              >
                         <CardCover2
                           sx={{
                             border: "1px solid",
